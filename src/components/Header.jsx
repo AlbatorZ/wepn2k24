0b204033a03e97767299afb6d1871df1ae23abb0
Abou Chebel Georgia <georgia.achebel@student-cs.fr>
2024-10-03 10:47:46 +0200

    Mise en forme du Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,16 @@
-export default function Header () {
-    
-    return (<header>
-        <h1> WEPN</h1>
-        </header>)
+import React from 'react';
+import './Header.css'; // Assure-toi que le fichier CSS est importé
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h1>Shotgun WEPN</h1>
+            <img 
+                src="/logo512.png" 
+                alt="WACS" 
+                style={{ width: '300px', height: '300px', marginRight: '10px' }}
+            />
+            <p>Le WEPN (Week-end Premières Neiges) aura lieu du 30 novembre au 1er décembre 2024 wouhouw</p>
+        </header>
+    );
 }
