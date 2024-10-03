6f450dc6261fdc46d103e8ab9ed24a342e9cf793
Corsaro Mattia <mattia.corsaro@student-cs.fr>
2024-10-03 20:02:10 +0200

    creation de container

---
+++ src/assets/tabElements.js
@@ -0,0 +1,9 @@
+import belle_image from '../assets/belle_image.jpg.avif'
+import plan_pistes from '../assets/plan_pistes.jpg'
+import bus from '../assets/bus_neige.jpg'
+
+export const tabElements = [
+    {titre: 'LE WEPN', description: "Le Wepn c'est incroyable vraiment", image : belle_image},
+    {titre: 'plan des pistes', description: " les prix seront compris entre", image : plan_pistes},
+    {titre: 'informations', description: "depart en bus à minuit, prevoyez des habits chauds", image : bus}
+]
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,21 @@
+import { tabElements } from "../assets/tabElements"
+import { useState } from "react"
+import './Container.css'
+
 export default function Container () {
-    return (<div>
-            <p> ici des infos avec un menu par exemple </p>
+    const [selectedButton, setSelectedButton] = useState(0)
+    return (<div id = 'tab'>
+            <menu>
+                {tabElements.map((element,index) => ( <button  key = {index} onClick={() => setSelectedButton(index)} alt = ''> {element.titre} </button>))}
+                
+                
+            </menu>
+            <div className="content">
+                <p>
+                    {tabElements[selectedButton].description}
+                    
+                </p>
+                <img src = {tabElements[selectedButton].image}></img>
+            </div>
         </div>)
 }
