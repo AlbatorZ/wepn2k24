4a8198f15e79bff0bee08e005eeb5b8620e1b639
Corsaro Mattia <mattia.corsaro@student-cs.fr>
2024-10-15 22:21:27 +0200

    nvl images et fix container

--- src/assets/tabElements.js
+++ src/assets/tabElements.js
@@ -1,10 +1,9 @@
-import belle_image from '../assets/belle_image.jpg.avif'
-import plan_pistes from '../assets/plan_pistes.jpg'
 import bus from '../assets/bus_neige.jpg'
+import alpe from '../assets/alpe_huez.jpg'
+import wacs_man from '../assets/belle-vue.jpg'
 
 export const tabElements = [
-    {bouton: 'Destination', titre : "Cette année, la WACS vous emmène à L'ALPE D'HUEZ", description: "250 km de pistes et de poudreuse s'offrent à vous. Que vous soyez des pros ou des débutants.tes venez profiter de cette offre incroyable!", image: belle_image},
-    {bouton: 'Prix', titre : "153€ Tarif normal et 89€ Tarif boursier", description : "Le prix comprend forfait, logement et transport. La WACS vous propose aussi des options de matériel et de pack bouffe réservables ultérieurement.", image : belle_image},
-    {bouton: 'Plan des pistes', titre : "", description: "Les prix : 153€ Tarif normal et 89€ Tarif boursier", image : plan_pistes},
-    {bouton: 'Informations', titre : "", description: "depart en bus à minuit, prevoyez des habits chauds", image : bus}
+    {bouton: 'Destination', titre : "Cette année, la WACS vous emmène à L'ALPE D'HUEZ", description: "250 km de pistes et de poudreuse s'offrent à vous. Que vous soyez des pros ou des débutants.tes venez profiter de cette offre incroyable!", image: alpe},
+    {bouton: 'Prix', titre : "153€ Tarif normal et 89€ Tarif boursier", description : "Le prix comprend forfait, logement et transport. La WACS vous propose aussi des options de matériel et de pack bouffe réservables ultérieurement.", image : wacs_man},
+    {bouton: 'Transport', titre : "Informations Importantes", description: "Depart en bus à minuit, prevoyez des habits chauds. La place dans la soute est limitée, il faut prévoir un sac et son matos mais pas plus", image : bus}
 ]
